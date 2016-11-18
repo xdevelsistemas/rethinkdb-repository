96d706eb0a5e5b571df7f6a98d894aa48f168015
Fabio Henriques <fabio.henriques@xdevel.com.br>
2016-11-18 09:50:33 -0200

    Renaming files to npm convetion

--- index.Spec.ts
+++ index.Spec.ts
@@ -1,7 +1,7 @@
 import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
 import * as util from 'gulp-util'
-import {Database} from './Database'
+import {Database} from './index'
 import * as r from 'rethinkdb'
 
 export class DatabaseTest {
